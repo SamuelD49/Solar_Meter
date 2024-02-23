9465ad14f0aacf6755e5a95814c126645f8f657a
Samuel Dereje <79600467+SamuelD49@users.noreply.github.com>
2024-02-23 09:16:42 -0800

    Update

--- lib/ass.dart
+++ lib/ass.dart
@@ -1,14 +1,1 @@
-import 'package:flutter/material.dart';
 
-class Ass extends StatelessWidget {
-  const Ass({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Text('ass'),
-      ),
-    );
-  }
-}
